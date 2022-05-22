0887686ff8ceafb5b0f2b19f862d023a0c401bd5
Aelida <lidasouza89@gmail.com>
2022-05-22 19:18:55 -0300

    Inclusão Resolução Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,13 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.some((manager) => manager === id));
 }
-
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(id) {
+  if (!isManager(id)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((employee) => employee.managers.some((manager) => manager === id))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
-
 module.exports = { isManager, getRelatedEmployees };
