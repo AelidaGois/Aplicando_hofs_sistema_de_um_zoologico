7bff41795293bd7683bd6491369f609fc74c81e3
Aelida <lidasouza89@gmail.com>
2022-05-23 21:52:03 -0300

    Resolução Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,26 +1,25 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
 
 function countEntrants(entrants) {
   const child = entrants.filter((entrant) => entrant.age < 18).length;
-  const adult = entrants.filter((entrant) => entrant.age >= 18 || entrant.age < 50).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
   const senior = entrants.filter((entrant) => entrant.age >= 50).length;
 
   return { child, adult, senior };
-} console.log(countEntrants(entrants));
+}
 
 function calculateEntry(entrants) {
   if (!entrants || !entrants[0]) {
     return 0;
   }
+  const count = countEntrants(entrants);
+
+  const totalAdult = prices.adult * count.adult;
+  const totalChild = prices.child * count.child;
+  const totalSenior = prices.senior * count.senior;
+  const total = totalAdult + totalChild + totalSenior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
