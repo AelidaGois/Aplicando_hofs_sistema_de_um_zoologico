bee0577fda1b5e46fc554162811d96069af58c29
Aelida <lidasouza89@gmail.com>
2022-05-25 21:34:03 -0300

    Inclusão resolução requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+function getEmployeesCoverage(id, firstName, lastName) {
+  const findEmployeesById = data.employees.find((employe) => employe.id === id);
+
+  return findEmployeesById;
+}console.log(getEmployeesCoverage('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+function getAnimalsByDay(day) {
+  if (day === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+
+  const filterAnimal = species.filter((specie) => specie.availability.includes(day))
+    .map((specie) => specie.name);
+
+  return filterAnimal;
 }
 
+function getHours(days) {
+  if (days === 'Monday') {
+    return 'CLOSED';
+  }
+  const hours1 = hours[days];
+  return `Open from ${hours1.open}am until ${hours1.close}pm`;
+}
+
+function createRecord(weekDay) {
+  return {
+    officeHour: getHours(weekDay),
+    exhibition: getAnimalsByDay(weekDay),
+  };
+}
+
+function getSchedule(scheduleTarget) {
+  const findAnimal = data.species.find(({ name }) => name === scheduleTarget);
+
+  if (findAnimal) {
+    return findAnimal.availability;
+  }
+
+  let weekDays = Object.keys(hours);
+
+  if (weekDays.includes(scheduleTarget)) {
+    weekDays = [scheduleTarget];
+  }
+
+  return weekDays.reduce((acc, daysCurr) => {
+    if (!acc[daysCurr]) {
+      acc[daysCurr] = createRecord(daysCurr);
+    }
+    return acc;
+  }, {});
+}
+console.log(getSchedule());
 module.exports = getSchedule;
